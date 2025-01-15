483d09035798911e28fc64dd2b3a239ae69deb73
Wasabi Hasashi <138564824+wasabi2710@users.noreply.github.com>
2025-01-15 17:28:51 +0700

    Update socket_wind.c

--- src/socket_wind.c
+++ src/socket_wind.c
@@ -1,9 +1,9 @@
-#include <winsock2.h>
 #include <ws2tcpip.h>
+#include <winsock2.h>
 #include <stdio.h>
 #include "headers/socket_common.h"
 
-void init_socket() {
+int init_socket() {
     WSADATA wsaData;
     SOCKET sockfd;
 
@@ -15,20 +15,64 @@ void init_socket() {
 
     sockfd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
     if (sockfd == INVALID_SOCKET) {
-        printf("Socket creation failed. Error code: %d\n", WSAGetLastError);
+        printf("Socket creation failed. Error code: %d\n", WSAGetLastError());
         WSACleanup();
         return 1;
     }
 
     printf("Socket created successfully\n");
 
-    closesocket(sockfd);
+    return sockfd;
 }
 
-int main() {  
+int main() { 
+    SOCKET sockfd, connfd; 
+    struct sockaddr_in serverAddr, clientAddr;
+    socklen_t clientAddrLen = sizeof(clientAddr);
+    char clientIp[INET_ADDRSTRLEN];
 
     printf("Socket on Windows\n");
-    init_socket();
+    sockfd = init_socket();
+
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(1234);
+    serverAddr.sin_addr.s_addr = INADDR_ANY;
+    
+    // bind socket
+    if (bind(sockfd, (struct sockaddr *)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR) {
+        perror("Failed bind");
+        closesocket(sockfd);    
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Binded successfully!\n");
+
+    // listen for connection
+    if (listen(sockfd, 5) == SOCKET_ERROR) {
+        perror("Failed listening");
+        closesocket(sockfd);
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Server listening on :1234 ...\n");
+
+    // acceptance
+    while (1) {
+
+        connfd = accept(sockfd, (struct sockaddr *)&clientAddr, &clientAddrLen);
+        if (connfd == SOCKET_ERROR) {
+            perror("Failed acceptitng connection");
+            continue;
+        }
+
+        //connection str conversion
+        inet_ntop(AF_INET, &clientAddr.sin_addr, clientIp, INET_ADDRSTRLEN);
+        printf("Connection from %s:%d\n", clientIp, ntohs(clientAddr.sin_port));
+
+        closesocket(connfd);
+    }
+
+    closesocket(sockfd);
 
     return 0;
 }
