24e5b4ef081f4c6397670848fc5176b01b9f6c8e
wasabi2710 <em.vannin.2001@gmail.com>
2025-02-10 12:06:32 +0700

    more

--- src/posix/clipboard.c
+++ src/posix/clipboard.c
@@ -2,57 +2,78 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+#ifdef CLIPBOARD_PBCOPY
+#include <stdlib.h>
+
+char* clipboard() {
+    // For macOS, use pbpaste
+    FILE *fp = popen("pbpaste", "r");
+    if (fp == NULL) {
+        perror("Failed to run pbpaste");
+        return NULL;
+    }
+
+    // Find the size of the clipboard content
+    fseek(fp, 0, SEEK_END);
+    long size = ftell(fp);
+    fseek(fp, 0, SEEK_SET);
+
+    // Read the content from the clipboard
+    char* result = (char*)malloc(size + 1);
+    if (result != NULL) {
+        fread(result, 1, size, fp);
+        result[size] = '\0'; // Null-terminate
+    }
+
+    fclose(fp);
+    return result;
+}
+#else
 #include <X11/Xlib.h>
 #include <X11/Xutil.h>
 #include <X11/keysym.h>
 
 char* clipboard() {
-    // Open the X11 display
     Display* display = XOpenDisplay(NULL);
     if (display == NULL) {
         perror("Unable to open X display");
         return NULL;
     }
 
-    // Get the root window and clipboard selection
     Window root = DefaultRootWindow(display);
     Atom clipboardAtom = XInternAtom(display, "CLIPBOARD", False);
     Atom targetAtom = XInternAtom(display, "UTF8_STRING", False);
     Atom propertyAtom = XInternAtom(display, "CLIPBOARD", False);
 
-    // Request clipboard content
     XConvertSelection(display, clipboardAtom, targetAtom, propertyAtom, root, CurrentTime);
     XFlush(display);
 
-    // Wait for the clipboard content to be available
     XEvent event;
     XNextEvent(display, &event);
 
-    // If clipboard content is available, fetch the data
     if (event.xselection.property != None) {
         Atom type;
         int format;
         unsigned long len, bytesAfter;
         unsigned char* data = NULL;
 
-        // Get the clipboard data
         if (XGetWindowProperty(display, root, event.xselection.property, 0, ~0L, False, AnyPropertyType,
                                &type, &format, &len, &bytesAfter, &data) == Success) {
             if (data != NULL) {
-                // Allocate space for the clipboard content and copy it
                 char* result = (char*)malloc(len + 1);
                 if (result != NULL) {
                     memcpy(result, data, len);
-                    result[len] = '\0'; // Null-terminate the string
-                    XFree(data); // Free the X11 data
-                    XCloseDisplay(display); // Close the X11 display
+                    result[len] = '\0';
+                    XFree(data);
+                    XCloseDisplay(display);
                     return result;
                 }
             }
         }
     }
 
-    // Cleanup and return NULL if clipboard content isn't found
     XCloseDisplay(display);
     return NULL;
 }
+#endif
